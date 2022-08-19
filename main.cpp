3fd6e9a82a8ef89374714500921761aed38ab846
JasonSolace <jasonarmenta02@gmail.com>
2022-08-19 01:12:12 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,18 +7,20 @@ Conways Game of Life with Raylib
 
 int main() {
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE); //We allow the user to resize. --Can maybe cause some bugs
-	InitWindow(1920, 1020, "Game of Life"); //Window size, title of program
+	InitWindow(1450, 700, "Game of Life"); //Window size, title of program
+	SetWindowMinSize(1450, 700);
 	SetTargetFPS(FLAG_VSYNC_HINT); //VSYNC of individuals monitor
 	Menu menu; //Although called menu, this class ontop of handling menu also controls gamestates and board changes through the game
 	bool rulesPrompt = true;
-	float splashTimer = 0.0; 
+	float splashTimer = 0.0;
 	const char* conwaysRules = "Conways Game of Life Rules\n1. Any live cell with fewer than two live nieghbors dies, as if by underpopulation\n"
 		"2. Any live cell with two or three live neighbors lives on to the next generation.\n"
 		"3. Any live cell with more than three live neighbors dies, as if by overpopulation";
 	const char* warRules = "War Edition Rules\n1. Any live cell with any enemy neighbors will not reproduce\n"
 		"2. If there are more enemy neighbors than allies, cell will automatically die";
 	const char* controls = "Controls\nLeft Click to spawn black cell.\nRight Click to spawn red cell\nMiddle click to delete\nSpacebar to run or pause simulation";
-
+	const char* clickToContinue = "CLICK ANYWHERE TO CONTINUE";
+	int introTextWidth = MeasureText(clickToContinue, 50);
 	char blackScore[5], redScore[5];
 
 	Texture2D logo = LoadTexture("resources/attempt.png");
@@ -27,7 +29,7 @@ int main() {
 
 	while (!WindowShouldClose())
 	{
-		if (splashTimer > 100) 
+		if (splashTimer > 100)
 		{
 			menu.ChangeMenu(); //Has a "one time use" where menu is over.
 			if (!rulesPrompt) {
@@ -40,14 +42,14 @@ int main() {
 				if (menu.GetMenu()) {
 					ClearBackground(ColorFromNormalized({ 1.0, 0.52, 0.54, 1.0 }));
 					DrawTexture(title, GetScreenWidth() / 2 - (1200 / 2), GetScreenHeight() / 2 - (900 / 2), WHITE);
-					DrawTextEx(font, "CLICK ANYWHERE TO CONTINUE", Vector2{ (float)GetScreenWidth() / 2 - 150, (float)GetScreenHeight() / 2 + 50 }, 32, 0, BLACK);
+					DrawTextEx(font, clickToContinue, Vector2{ (float)GetScreenWidth() / 2 - (introTextWidth / 2), (float)GetScreenHeight() - 100 }, 50, 0, BLACK);
 				}
 				else {
 					ClearBackground(ColorFromNormalized({ 1.0, 0.52, 0.54, 1.0 }));
-					DrawTextEx(font, conwaysRules, Vector2{ 100 , 100 }, 32, 0, BLACK);
-					DrawTextEx(font, warRules, Vector2{ 100 , 300 }, 32, 0, BLACK);
-					DrawTextEx(font, controls, Vector2{ (float)GetScreenWidth() / 2 - 300, (float)GetScreenHeight() / 2 }, 32, 0, BLACK);
-					DrawTextEx(font, "CLICK ANYWHERE TO CONTINUE", Vector2{ (float)GetScreenWidth() / 2 - 400, (float)GetScreenHeight() - 250 }, 50, 0, BLACK);
+					DrawTextEx(font, conwaysRules, Vector2{ 35 , 100 }, 32, 0, BLACK);
+					DrawTextEx(font, warRules, Vector2{ 35 , 300 }, 32, 0, BLACK);
+					DrawTextEx(font, controls, Vector2{ (float)GetScreenWidth() / 2 - MeasureText(controls, 50) / 2, (float)GetScreenHeight() / 2 + 150 }, 32, 0, BLACK);
+					DrawTextEx(font, clickToContinue, Vector2{ (float)GetScreenWidth() / 2 + introTextWidth / 2 - 200, (float)GetScreenHeight() - 100 }, 35, 0, BLACK);
 					if (IsMouseButtonPressed(0))
 						rulesPrompt = false;
 				}
@@ -61,7 +63,7 @@ int main() {
 				DrawText(redScore, GetScreenWidth() - 110, 30, 50, RED);
 
 			}
-			
+
 			EndDrawing(); //End of drawing
 		} //Runs JasonSolace intro for a bit then moves on. Might need to make this better - different vsyncs can cause this to be an issue!
 		else
